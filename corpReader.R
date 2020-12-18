4e5deb59ecaa796904421f4903589162f260f5a1
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2020-12-17 22:47:58 -0500

    updated corp reader to initially read in example data

--- corpReader.R
+++ corpReader.R
@@ -1,2 +1,18 @@
-corp <-
-  readtext("E:\Default\OneDrive - nyu.edu\Coursework\Fall 2020\Building Learning Analytics Applications\BLA5\corpora\archive\spanishText_10000_15000")
+library(rvest)
+library(stringr)
+library(dplyr)
+DATA_DIR <- read_html(paste0(getwd(), "/data/spanish_corpus/practiceText"))
+
+DATA_nodes <- DATA_DIR %>%
+  html_nodes("doc") 
+
+DATA_text <- DATA_nodes %>%
+  html_text(trim = TRUE) %>%
+  str_replace_all("\n+", "") 
+
+DATA_attr <- DATA_nodes %>%
+  html_attrs() %>%
+  bind_rows()
+
+corpClean <- tibble(text = DATA_text) %>%
+  bind_cols(DATA_attr, .)
