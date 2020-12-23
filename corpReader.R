293159ae47041995d104b99b62505175d5cdc31c
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2020-12-22 23:34:14 -0500

    commented, cleaned variable names

--- corpReader.R
+++ corpReader.R
@@ -1,18 +1,27 @@
+### INSTALL LIBRARIES ####
 library(rvest)
 library(stringr)
 library(dplyr)
-DATA_DIR <- read_html(paste0(getwd(), "/data/spanish_corpus/practiceText"))
 
-DATA_nodes <- DATA_DIR %>%
-  html_nodes("doc") 
+### CORPUS READING + CLEANING ####
+# set directory
+DATA_DIR <- paste0(getwd(), "/data/spanish_corpus/practiceText")
 
+# read in HTML
+DATA_HTML <- read_html(DATA_DIR)
+
+# read in nodes + build DATA_text
+DATA_nodes <- DATA_HTML %>% 
+  html_nodes("doc")
 DATA_text <- DATA_nodes %>%
   html_text(trim = TRUE) %>%
-  str_replace_all("\n+", "") 
+  str_replace_all("\n+", "") # replacing newlines
 
+# read in attributes + build DATA_attr
 DATA_attr <- DATA_nodes %>%
   html_attrs() %>%
   bind_rows()
 
-corpClean <- tibble(text = DATA_text) %>%
+# combine DATA_attr + DATA_text into DATA_clean
+DATA_clean <- tibble(text = DATA_text) %>%
   bind_cols(DATA_attr, .)
