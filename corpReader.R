893f7fa2dfde7d5c3ae1fb73ed63866b1412be92
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2020-12-23 00:46:53 -0500

    started removal list

--- corpReader.R
+++ corpReader.R
@@ -5,17 +5,31 @@ library(dplyr)
 
 ### CORPUS READING + CLEANING ####
 # set directory
-DATA_DIR <- paste0(getwd(), "/data/spanish_corpus/practiceText")
+DATA_DIR <-
+  paste0(getwd(), "/exampleData/spanish_corpus/practiceText") # TODO update to read in ALL files
 
-# read in HTML
+# read in HTML of corpus
 DATA_HTML <- read_html(DATA_DIR)
 
-# read in nodes + build DATA_text
-DATA_nodes <- DATA_HTML %>% 
+# read in nodes + build DATA_text, replace
+DATA_nodes <- DATA_HTML %>%
   html_nodes("doc")
 DATA_text <- DATA_nodes %>%
-  html_text(trim = TRUE) %>%
-  str_replace_all("\n+", "") # replacing newlines
+  html_text(trim = TRUE)
+
+# remove irrelevant text and newlines
+DATA_DIR <- paste0(getwd(), "/cleaning/corpusRemoval.txt")
+
+# read in removal list as character vector
+REMOVE_LIST <- readLines(DATA_DIR)
+
+# FIXME remove a series of words form a list (formatting, numbers, months, etc.)
+# DATA_text <- DATA_text %>%
+#   str_replace_all(pattern = REMOVE_LIST, replacement = "")
+
+# remove new lines from DATA_text
+DATA_text <- DATA_text %>%
+  str_replace_all("\n+", "")
 
 # read in attributes + build DATA_attr
 DATA_attr <- DATA_nodes %>%
